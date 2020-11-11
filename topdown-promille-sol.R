13a2e9bf3575aa943cd4088c7a4d6d6186d5f233
Lisa Wimmer <wml1293@outlook.de>
2020-11-11 22:24:30 +0100

    Implemented alc formula

--- topdown-promille-sol.R
+++ topdown-promille-sol.R
@@ -20,14 +20,40 @@ tell_me_how_drunk <- function(age,
     ethanol = c(0.06, 0.06, 0.11, 0.4)
   )
   
-  drinks_df <- data.frame(drinks) %>% 
+  amount_consumed <- data.frame(drinks) %>% 
     gather() %>% 
     left_join(lookup_alc, by = "key") %>% 
-    rename("drink" = key, "amount" = value) %>% 
-    mutate(amount_alc = amount * volume * ethanol * 0.8)
-
+    mutate(amount_alc = value * volume * ethanol * 0.8) %>% 
+    summarise(sum(amount_alc)) %>% 
+    as.numeric()
+  
+  get_gkw <- function(sex, age, height, weight) {
+    
+    male <- sex == "male"
+    intercept <- ifelse(male, 2.447, 0.203)
+    b_age <- ifelse(male, -0.09516, -0.07)
+    b_height <- ifelse(male, 0.1074, 0.1069)
+    b_weight <- ifelse(0.3362, 0.2466)
+    
+    intercept + b_age * age + b_height * height + b_weight * weight
+    
+  }
+  
+  get_detox <- function(drinking_time) 0.15 * (diff(drinking_time) - 1)
+  
+  gkw <- get_gkw(sex, age, height, weight)
+  detox <- get_detox(drinking_time)
+  
+  0.8 * amount_consumed / (1.055 * gkw) - detox
   
 }
 
 drinks <- list("massn" = 2, "schnaps" = 3)
-data.frame(drinks) %>% gather()
+tell_me_how_drunk(
+  age = 26, 
+  sex = "female", 
+  height = 174, 
+  weight = 65, 
+  drinks = drinks,
+  drinking_time = c(2, 6)
+)
